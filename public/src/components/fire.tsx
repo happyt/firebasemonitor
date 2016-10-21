f7bbdb009874fe38a2e8cb733dc084c720614b07
Ian Morton <ian.morton@sky.uk>
2016-10-21 19:02:51 +0100

    added a timer

---
+++ public/src/components/fire.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export interface Props {  
+}
+
+export default class Timer extends React.Component<Props, {}> {
+  constructor(props) {
+    super(props);
+    this.state = {secondsElapsed: 0};
+  }
+
+  tick() {
+    this.setState((prevState) => ({
+      secondsElapsed: prevState.secondsElapsed + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
+    );
+  }
+}
---
+++ public/src/components/fire1.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+var ListItems = React.createClass({
+  render: function() {
+    var _this = this;
+    var createItem = function(item, index) {
+      return (
+        <li key={ index }>
+          { item.text }
+        </li>
+      );
+    };
+    if (Array.isArray(this.props.items) && this.props.items.length > 0) {
+//        console.log("Length...", this.props.items.length);
+        return <ul>{ this.props.items.map(createItem) }</ul>;
+    } else {
+        return <div>No entries</div>
+    }
+  }
+});
+
+var Fire = React.createClass({
+    getInitialState: function() {
+        return {
+            items : []
+        };
+    },
+
+  componentWillMount: function() {
+    this.firebaseRef  = firebase.database().ref("table");
+    this.firebaseRef.on('value', function(dataSnapshot) {
+        var items = [];
+        dataSnapshot.forEach(function(childSnapshot) {
+            var item = childSnapshot.val();
+            item['.key'] = childSnapshot.key;
+            items.push(item);
+        }.bind(this));
+        this.setState({
+             items: items
+         });
+     }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.firebaseRef.off();
+  },
+
+  render: function() {        
+        return(
+            <div>
+                <div>Firebase table:</div>
+                <ListItems items={ this.state.items } /> 
+           </div>  
+        );
+    }
+});
+
+module.exports = Fire;
